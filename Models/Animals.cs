84d0678495e2fcc4b4ea007b9a94c808b163c3ea
Denis Sabri <dsabri@tu-sofia.bg>
2022-08-01 16:07:19 +0300

    .

---
+++ Models/Animals.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternshipProject.Models
+{
+    public class Animals
+    {
+        public int id { get; set; }
+       // public string animal_type { get; set; }
+
+        public Animals()
+        {
+
+        }
+    }
+}
